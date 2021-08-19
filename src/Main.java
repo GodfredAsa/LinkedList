d416cc7392002fb715447c240f9bd1d9d104d547
Godfred Asumadu Asamoah <godfred.asumadu.asamoah@gmail.com>
2021-08-19 13:29:27 +0000

    adds visit method for navigating through the linked lists of cities

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
 
@@ -74,5 +75,47 @@ public class Main {
         }
         stringListIterator.add(newCity);
         return true;
+    }
+
+    private static void visit(LinkedList cities){
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        ListIterator<String> listIterator = cities.listIterator();
+        if(cities.isEmpty()){
+            System.out.println("No cities available");
+            return;
+        }else{
+            System.out.println("Now visiting " + listIterator.next());
+            printMenu();
+        }
+          while(!quit){
+              int action  = scanner.nextInt();
+              scanner.nextLine();
+              switch(action){
+                  case 0:
+                      System.out.println("Holiday (vacation) over");
+                      quit  = true;
+                      break;
+
+                  case 1:
+                      if(listIterator.hasNext()){
+                          System.out.println("Now visiting " + listIterator.next());
+                      }else{
+                          System.out.println("Reached the end of the list");
+                      }
+                      break;
+
+                  case 2:
+                      if(listIterator.hasPrevious()){
+                          System.out.println("Now visiting " + listIterator.previous());
+                      }else{
+                          System.out.println("We are at the start of the lists");
+                      }
+                      break;
+                  case 3:
+                      printMenu();
+                      break;
+              }
+          }
     }
 }
