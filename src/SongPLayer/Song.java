54a8be57893c6f20df47e9e798400ff3a76794c6
Godfred Asumadu Asamoah <godfred.asumadu.asamoah@gmail.com>
2021-08-20 17:51:14 +0000

    comments the methods in the Song.java

--- src/SongPLayer/Song.java
+++ src/SongPLayer/Song.java
@@ -3,13 +3,19 @@ package SongPLayer;
 public class Song {
     private String title;
     private double duration;
+
+    // constructor
     public Song(String title, double duration) {
         this.title = title;
         this.duration = duration;
     }
+
+    // getter
     public String getTitle() {
         return title;
     }
+
+    // overridding of the toString()
     @Override
     public String toString() {
         return this.title + ": " + this.duration;
