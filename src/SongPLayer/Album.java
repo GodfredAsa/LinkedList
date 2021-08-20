3aaa29105ebfa0479d0f009233cef0e7c938758e
Godfred Asumadu Asamoah <godfred.asumadu.asamoah@gmail.com>
2021-08-20 18:15:22 +0000

    comments the methods of the Album class

--- src/SongPLayer/Album.java
+++ src/SongPLayer/Album.java
@@ -7,11 +7,15 @@ public class Album {
     private String name;
     private String artist;
     private ArrayList<Song> songs;
+
+    // constructor
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
         this.songs = new ArrayList<Song>();
     }
+
+    // adding a song to the album
     public boolean addSong(String title, double duration) {
         if(findSong(title) == null) {
             this.songs.add(new Song(title, duration));
@@ -19,6 +23,8 @@ public class Album {
         }
         return false;
     }
+
+    // finding a song method
     private Song findSong(String title) {
         for(Song checkedSong: this.songs) {
             if(checkedSong.getTitle().equals(title)) {
@@ -27,6 +33,8 @@ public class Album {
         }
         return null;
     }
+
+    // add song to playList method
     public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
         int index = trackNumber -1;
         if((index >0) && (index <= this.songs.size())) {
@@ -36,6 +44,8 @@ public class Album {
         System.out.println("This album does not have a track " + trackNumber);
         return false;
     }
+
+    // addSongToPlayList overriden
     public boolean addToPlayList(String title, LinkedList<Song> playList) {
         Song checkedSong = findSong(title);
         if(checkedSong != null) {
